3a1f577b14218440abe76af5e2478c94ace69f24
selletrice <escanlarlille@gmail.com>
2026-05-03 16:57:53 +0700

    feat: define student struct and framework in visa_pro.h

---
+++ visa_pro.h
@@ -0,0 +1,30 @@
+#ifndef VISA_PRO_H
+#define VISA_PRO_H
+
+#define MAX 100
+
+// Node for Student Profile and Hashing
+struct stageNode{
+    struct stageNode *next;
+};
+
+// Node for Tree
+struct docNode{
+    char docName[MAX];
+    int isCollected; // 0 for false, 1 for true
+    struct docNode *right;
+    struct docNode *left;
+};
+
+// The Student Profile
+struct student{
+    long long id;
+    char name[MAX];
+    char visaExpiry[20]; // YYYY-MM-DD
+    char report90DayDeadline[20]; // 90-Day Timer
+
+    struct stageNode *currentStage; // Member 1: Points to current Linked List node
+    struct docNode *docTreeRoot; // Member 2: Points to root of Document Tree
+};
+
+#endif
