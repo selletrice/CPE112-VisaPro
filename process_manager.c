dec997d3a8bc84f908d8718c3e6c53a25f20fcaa
selletrice <escanlarlille@gmail.com>
2026-05-04 11:51:07 +0700

    implemented the framework of renewVisaProcess function to restart the entire roadmap

--- process_manager.c
+++ process_manager.c
@@ -5,7 +5,7 @@
 
 struct stageNode *headStage = NULL; //Global pointer for start or end of the roadmap
 
-void initRoadmap(){
+void initVisaRoadmap(){
     // Create new nodes for each stage
     struct stageNode *s1 = (struct stageNode*)malloc(sizeof(struct stageNode));
     struct stageNode *s2 = (struct stageNode*)malloc(sizeof(struct stageNode));
@@ -26,4 +26,16 @@ void initRoadmap(){
 
     // Set stage 1 as the head (very first) stage
     headStage = s1;
+}
+
+void renewVisaProcess(struct student *s, char* newExpiry){
+    // Update the pointer to point back to the beginning, thus resetting the process
+    strncpy(s->visaExpiry, newExpiry, 20);
+    s->currentStage = headStage; // Reset the Linked List pointer to Stage 1
+
+    // Member 2 Task: Call a function here to clear their Document Tree
+    // Member 3 Task: Update their 90-day reminder Priority Queue
+
+    printf("Visa Renewed for %s. New Expiry: %s. Process restarted.\n", s->name, s->visaExpiry);
+
 }
